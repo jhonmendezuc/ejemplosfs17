4c25f29f5d2d2d545515ecb955091e71fc152e0d
jhon mendez <fullstackuc@gmail.com>
2025-01-15 19:49:10 -0500

    feat:add todolist finish

--- HTMLYCSS/todolist/js/todo.js
+++ HTMLYCSS/todolist/js/todo.js
@@ -1,15 +1,16 @@
 let inputTask = document.getElementById("todo-input");
 let addBtn = document.getElementById("add-btn");
 let todoList = document.getElementById("todo-list");
+let allBtn = document.getElementById("filter-all");
+let completedBtn = document.getElementById("filter-completed");
+let activeBtn = document.getElementById("filter-active");
 
 let taskArray = [
   {
-    id: 1,
     title: "Go to the gym",
-    completed: false,
+    completed: true,
   },
   {
-    id: 2,
     title: "Go to the market",
     completed: true,
   },
@@ -17,19 +18,24 @@ let taskArray = [
 
 let currentTaskFilter = "all";
 
-let TaskFilter = taskArray.filter((task) => {
-  return true;
-});
-
 function showTask() {
-  TaskFilter.forEach((task) => {
+  todoList.innerHTML = "";
+  let TaskFilter = taskArray.filter((task) => {
+    if (currentTaskFilter === "active") return !task.completed;
+    if (currentTaskFilter === "completed") return task.completed;
+    return true;
+  });
+
+  TaskFilter.forEach((task, index) => {
     const li = document.createElement("li");
     li.innerHTML = `         
-          <div class="todo-item"> 
-            <input type="checkbox" />
+          <div class="todo-item ${task.completed ? "task-completed" : ""} "> 
+            <input data-index="${index}" type="checkbox" ${
+      task.completed ? "checked" : ""
+    } />
             <span>${task.title}</span>
           </div> 
-          <button><i class="bi bi-bookmark-x-fill"></i></button>
+          <button><i data-index="${index}" class="bi bi-bookmark-x-fill"></i></button>
         `;
     todoList.appendChild(li);
   });
@@ -41,8 +47,41 @@ addBtn.addEventListener("click", () => {
     completed: false,
   };
   taskArray.push(task);
+  console.log(taskArray);
   inputTask.value = "";
   showTask();
 });
 
-showTask();
+todoList.addEventListener("click", (evento) => {
+  let index = evento.target.dataset.index;
+  if (evento.target.tagName === "INPUT") {
+    console.log(evento.target.tagName);
+    //console.log(evento.target.dataset);
+    //console.log(taskArray[index]);
+    taskArray[index].completed = !taskArray[index].completed;
+    console.log(taskArray);
+  }
+  if (evento.target.tagName === "I") {
+    taskArray.splice(index, 1);
+  }
+
+  showTask();
+});
+
+allBtn.addEventListener("click", () => {
+  currentTaskFilter = "all";
+  console.log(currentTaskFilter);
+  showTask();
+});
+
+activeBtn.addEventListener("click", () => {
+  currentTaskFilter = "active";
+  console.log(currentTaskFilter);
+  showTask();
+});
+
+completedBtn.addEventListener("click", () => {
+  currentTaskFilter = "completed";
+  console.log(currentTaskFilter);
+  showTask();
+});
